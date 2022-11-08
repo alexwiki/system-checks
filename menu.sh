d057ae9f8c495f33536f13b4a6457d659c5a2135
alexwiki <alexstoubis@gmail.com>
2022-11-08 18:07:42 -0500

    version 2, without the implementation of the database (No-SQL)

--- menu.sh
+++ menu.sh
@@ -3,10 +3,9 @@
 
 ### TO-DO: ADD COLORS
 ###        ADD exit codes and fixes
-
  main()
 {
-    script -a my_draft_db.txt           #We keep the -a argument in order to append to the same file and NOT to delete it's history.
+    #script -a my_draft_db.txt           Den douleuei h mlkia ... #We keep the -a argument in order to append to the same file and NOT to delete it's history.
     
     #trying to figure out if the client wrote his name.  example: ./menu.sh Alex 
     if [ -z "$1"];
@@ -22,7 +21,23 @@
     
     echo "Bonjour! Please choose what would you like to know today about this system. 1 for system_status, 2 for who is logged in currently, 3 for checking updates, 4 for service ops, 5 for storage options"
     read user_input
-
+sadf
+    if ["$user_input"=1]; 
+    then
+    system_status
+    elif ["$user_input"=2];
+    then
+    who_is_in
+    elif ["$user_input"=3];
+    then
+    update_system
+    elif ["$user_input"=4];
+    then
+    service_check
+    elif ["$user_input"=5];
+    then
+    storage_check
+    fi
    
 }
 
@@ -61,19 +76,70 @@ who_is_in()         #OPTION 2
 
 update_system()     #OPTION 3
 {
-
     echo "Checking System Updates..."
+
+    sudo apt update                  #checks any updates and goes forward to update the software.
+
+    sudo apt-get dist-upgrade       #checks and moves forward if you want to upgrade your system.
+
+    sudo apt upgrade kernel         #checks and moves forward if you want to upgrade the kernel.
 
 }
 
 service_check()      #OPTION 4
 {  
+    echo "Write the service's name to be checked."
+    read service
     echo "Checking Services..."
+
+    sudo service $service Status
+
+    echo "What to do with the service? 1: Restart, 2: Stop, 3: Enable, 4: Disable, 5: start"
+    read service_choice
+    if [service_choice== 1];
+    then 
+    sudo service $service restart
+    
+    elif [service_choice== 2];
+    then
+    sudo service $service stop
+    
+    elif [service_choice== 3];
+    then
+    sudo service $service enable
+    elif [service_choice== 4];
+    then
+    sudo service $service disable
+    elif [service_choice== 5];
+    then
+    sudo service $service start
+    fi
+
+
 }   
 
 storage_check()     #OPTION 5
 {
     echo "Checking Storage..."
+
+    echo "Listing All partitions and mountpoints..."
+    lsblk
+    echo "Listing All partitions in an alternative way..."
+    sudo fdisk -l
+    echo "Checking partitions labeled by UUIDS"
+    blkid |grep UUID            #alternative way: grep UUID /etc/fstab
+
+    echo "Checking disk used by user..."
+
+    du -h /home
+    echo "specify the username:"
+    read username
+
+    du -h /home/$username
+
+
+
+
 }
 
 
@@ -88,3 +154,4 @@ store_to_db()      #Probably make a function which saves into a .json file the o
 
 
 main
+
